ad0bf38d36c5f617db2042e95dc5cf847a49d410
Marcos Cardoso <marcosvinimf@gmail.com>
2025-09-20 22:05:52 -0300

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *array;
+    int tamanho;
+    
+    printf("Digite a quantidade de valores a ser lido:");
+    scanf("%d", &tamanho);
+
+    array = (int *)malloc(tamanho * sizeof(int));
+    
+    if (array == NULL){
+        printf("Não foi possível alocar memória.");
+        return 1;
+    }
+
+    for (int i = 0; i < tamanho; i++){
+        printf("Número %d: ", i+1);
+        scanf("%d", &array[i]);
+    }
+    
+    printf("\n");
+
+    for (int i = 0; i < tamanho; i++){
+        printf("Número %d: %d\n", i+1, array[i]);
+    }
+
+    free(array);
+
+    return 0;
+}
